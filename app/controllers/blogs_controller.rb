90ea4d5ac0e649e6b5322d5332917186047e54cc
ヨシダタイキ <abc521fjr7@gmail.com>
2021-04-05 21:07:46 +0900

    [Fix]showの修正

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -5,8 +5,6 @@ class BlogsController < ApplicationController
 
   def show
     @blog = Blog.find(params[:id])
-    
-    redirect_to blog_path(blog.id)
   end
 
   def new
@@ -16,7 +14,7 @@ class BlogsController < ApplicationController
   def create
     blog = Blog.new(blog_params)
     blog.save
-    redirect_to blogs_path
+    redirect_to blog_path(blog.id)
   end
 
   def edit
